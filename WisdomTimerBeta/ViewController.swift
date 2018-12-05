f6a0153f42111cc1352832dd61d1bf0637cee209
田中惇貴 <tanakaatsuki@tanakaatsushitakanomacbook-puro.local>
2018-12-05 20:59:23 +0900

    Initial Commit

---
+++ WisdomTimerBeta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WisdomTimerBeta
+//
+//  Created by 田中惇貴 on 2018/12/05.
+//  Copyright © 2018 田中惇貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
